443613e98e488232286f97a89552e4ad40882f6d
GarySimons <garycsimons@gmail.com>
2026-07-15 14:13:04 +0100

    side-cart-item added for sidebar

--- assets/theme.js
+++ assets/theme.js
@@ -4,3 +4,39 @@ document
     event.preventDefault();
     document.getElementById("drawer").open();
   });
+
+class SidebarDrawer extends HTMLElement {
+  connectedCallback() {
+    this.overlay = this.querySelector(".drawer-overlay");
+
+    this.overlay?.addEventListener("click", () => this.close());
+
+    document.addEventListener("keydown", this.handleKeydown);
+  }
+
+  disconnectedCallback() {
+    document.removeEventListener("keydown", this.handleKeydown);
+  }
+
+  handleKeydown = (event) => {
+    if (event.key === "Escape") {
+      this.close();
+    }
+  };
+
+  open() {
+    this.setAttribute("open", "");
+    document.body.style.overflow = "hidden";
+  }
+
+  close() {
+    this.removeAttribute("open");
+    document.body.style.overflow = "";
+  }
+
+  toggle() {
+    this.hasAttribute("open") ? this.close() : this.open();
+  }
+}
+
+customElements.define("sidebar-drawer", SidebarDrawer);
